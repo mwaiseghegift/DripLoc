2c664bd96f380c6033eea3e7ba0f91212bc3b004
Gateway47 <itsregalo047@gmail.com>
2021-03-31 17:22:27 +0300

    added receive data from ajax functionality

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, CountyDataView, IncidenceView
+from .views import IndexView, CountyDataView, IncidenceView, GetNearestDrip
 
 app_name = 'mainapp'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', IndexView, name='index'),
     path('counties/', CountyDataView, name='counties'),
     path('incidence/', IncidenceView, name='incidences'),
+    path('getshops/', GetNearestDrip, name='getshops')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from .models import Counties, Incidence
 from django.core.serializers import serialize
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.db.models.functions import Distance
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def IndexView(request, *args, **kwargs):
@@ -12,4 +16,28 @@ def CountyDataView(request, *args, **kwargs):
     return HttpResponse(counties, content_type='json')
 
 def IncidenceView(request, *args, **kwargs):
-    pass
+    incidences = serialize('geojson', Incidence.objects.all())
+    return HttpResponse(incidences, content_type='json')
+
+
+def GetNearestDrip(request, *args, **kwargs):
+    if request.method=='POST':
+        latitude1 = request.POST.get('latitude')
+        longitude1 = request.POST.get('longitude')
+
+        datao = 0.25455511
+        print(type(datao))
+
+        floatLatitude = float(latitude1)
+        floatLongitude = float(longitude1)
+        
+        user_location = Point((floatLongitude, floatLatitude), srid=4326)
+        queryset = Incidence.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
+
+        context = {
+            'latitude':floatLatitude,
+            'longitude':floatLongitude,
+            'queryset':queryset,
+        }
+        return render(request, 'nearest.html', context)
+    return render(request, 'nearest.html')
